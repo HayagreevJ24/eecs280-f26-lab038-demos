134046dd21f2db1beeefc61ec75fea02fcd37aa4
Hayagreev Jeyandran <hayagreevjeyandran054@gmail.com>
2026-09-17 00:43:47 -0400

    Lab 2 commit

---
+++ lab2/file1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+void foo() {
+    std::cout << "fooOOOO!!!1!\n"; 
+}
+
+
+int main() {
+    std::cout << "file1.cpp\n"; 
+}
---
+++ lab2/file2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "file1.hpp"
+
+int main() {
+    // TODO: Call foo() here in exercise 3
+    std::cout << "file2.cpp\n"; 
+
+}
